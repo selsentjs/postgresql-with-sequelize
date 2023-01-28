5d44a09a430dae03c55c0d7f57df711e06219ea7
Selvi <vsrfamily1234@gmail.com>
2023-01-28 23:42:05 +0530

    created migrations,modeland seeders

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const {Sequelize, sequelize} = require('./models');
 const app = express()
 
 app.use(express.json());
 
+async function main(){
+    await sequelize.sync()
+}
+main()
 app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
